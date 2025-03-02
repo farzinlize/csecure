e68a383b84d515546fc4e91396682e1cb0f63059
farzin <farzinlize@live.com>
2025-03-02 16:59:08 +0330

    use cmake configuration for sample size
    - unit test `simple-test` now uses defined size for testing a single message
    with configurable macro

--- unit-tests/simple-test.c
+++ unit-tests/simple-test.c
@@ -1,6 +1,8 @@
 #include"../includes/security.h"
 
-#define SINGLE_SIZE 102400
+#ifndef CMAKE_MAKE
+#define LIBCS_SINGLE_SIZE 102400
+#endif
 
 char * random_bytes(size_t length){
     char * r = (char *)malloc(length);
@@ -12,16 +14,16 @@ char * random_bytes(size_t length){
 int main(){
     keyring keys;
     size_t enc_count, dec_count;
-    char * simple = random_bytes(SINGLE_SIZE);
+    char * simple = random_bytes(LIBCS_SINGLE_SIZE);
 
     generate_rsa_keys(&keys);
     setup_other_key(&keys, keys.me_public_key);
     printf("keys are generated\n");
 
-    char * enc = encrypt_msg(keys, simple, SINGLE_SIZE, &enc_count);
+    char * enc = encrypt_msg(keys, simple, LIBCS_SINGLE_SIZE, &enc_count);
     printf("encrypted --- enc_length=%ld\n", enc_count);
     char * dec = decrypt_msg(keys, enc, enc_count, &dec_count);
     printf("decrypted\ndec_length=%ld, approved->%s\n",
-        dec_count, memcmp(simple, dec, SINGLE_SIZE)?"no":"yes"
+        dec_count, memcmp(simple, dec, LIBCS_SINGLE_SIZE)?"no":"yes"
     );
 }
