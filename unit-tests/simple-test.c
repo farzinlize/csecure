c5a355c5c994fac2f86d714ee463dfdb13d17d7e
farzin <farzinlize@live.com>
2025-03-13 09:13:34 +0330

    simple test modification to test if library can process data inside stack instead of heap

--- unit-tests/simple-test.c
+++ unit-tests/simple-test.c
@@ -11,10 +11,22 @@ char * random_bytes(size_t length){
     return r;
 }
 
+void random_bytes_nomem(char * where, size_t length){
+    for(int i=0;i<length;i++)
+        where[i] = (char) rand()%256;
+}
+
 int main(){
     keyring keys;
-    size_t enc_count, dec_count;
+    size_t enc_count, dec_count, en2_count;
     char * simple = random_bytes(LIBCS_SINGLE_SIZE);
+    char stack[LIBCS_SINGLE_SIZE];
+
+    printf("stack and simple compare:\n- stack\n");
+    fwrite(stack, 1, LIBCS_SINGLE_SIZE, stdout);
+    printf("\n- simple\n");
+    fwrite(simple, 1, LIBCS_SINGLE_SIZE, stdout);
+    if(memcmp(simple, stack, LIBCS_SINGLE_SIZE)) printf("\nequal approved\n");
 
     generate_rsa_keys(&keys);
     setup_other_key(&keys, keys.me_public_key);
@@ -22,6 +34,9 @@ int main(){
 
     char * enc = encrypt_msg(keys, simple, LIBCS_SINGLE_SIZE, &enc_count);
     printf("encrypted --- enc_length=%ld\n", enc_count);
+    char * en2 = encrypt_msg(keys, stack,  LIBCS_SINGLE_SIZE, &en2_count);
+    printf("stacked ok --- en2_length=%ld\n", en2_count);
+    
     char * dec = decrypt_msg(keys, enc, enc_count, &dec_count);
     printf("decrypted\ndec_length=%ld, approved->%s\n",
         dec_count, memcmp(simple, dec, LIBCS_SINGLE_SIZE)?"no":"yes"
